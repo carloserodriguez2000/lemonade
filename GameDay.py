8b470a55c5866226e8174c2090a8151043b27036
carloserodriguez2000 <carloserodriguez2000@yahoo.com>
2016-05-24 13:31:55 -0500

    Continue project setup. No usable code at this time

--- GameDay.py
+++ GameDay.py
@@ -1,1 +1,23 @@
-class GameDay
+class GameDay:
+
+    def __init__(self):
+
+
+    def playGame(self):
+        i=0
+        #Initialize Owner  Get name, age,....
+        #if player is already in the Score Database, read current balance $. MOre advance read all inventory also.
+        #if new player
+            # "Welcome new Owner"
+            # Create a new Owner Record
+            # Print some instructions
+            # Show initial seed Money. Or Ask.  How much Money Grandma Gave you to play?
+        #################
+        # Prompt "Lets buy some supplies"
+            # if skip and not enough supplies to buy Put WARNING message. Note: donations to start business.
+        #################
+
+        # BUY  items. posibly handle all inside the INVENTORY CLASS.
+            # inventoy.Buy(Owner)  buy lemons, ice, cups, sugar, water. Accounts for cash VS total cost to buy
+
+
